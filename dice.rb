de24c840a668d5462014216bc2cae624dc92ac00
tferrar0 <146215589+tferrar0@users.noreply.github.com>
2023-11-01 03:05:52 +0000

    added giraffe route

--- dice.rb
+++ dice.rb
@@ -8,3 +8,10 @@ end
 get("/zebra") do
   "We must add a route for each path we want to support"
 end
+
+require "sinatra/reloader"
+
+get("giraffe") do
+  "Hopefully this shows up without having to restart the server"
+end
+
